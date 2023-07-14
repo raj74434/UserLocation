13ca07bcd72dec8659922a23e673c01b620f3381
Raj Gupta <105916680+raj74434@users.noreply.github.com>
2023-07-13 23:21:39 -0700

    properties ,controller ,models and basic project

---
+++ ambula/src/main/java/com/userLocation/ambula/models/User_location.java
@@ -0,0 +1,11 @@
+package com.userLocation.ambula.models;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User_location {
+    private String NAME;
+    private Latitude;
+    private Longitude;
+
+}
---
+++ ambula/src/test/java/com/userLocation/ambula/AmbulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userLocation.ambula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
