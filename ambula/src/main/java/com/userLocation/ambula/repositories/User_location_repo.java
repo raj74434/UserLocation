61374916078c70836c763850a5dc1d4fd6e62b04
Raj Gupta <105916680+raj74434@users.noreply.github.com>
2023-07-14 20:30:27 +0530

    Update User_location_repo.java

--- ambula/src/main/java/com/userLocation/ambula/repositories/User_location_repo.java
+++ ambula/src/main/java/com/userLocation/ambula/repositories/User_location_repo.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 
-public interface User_location_repo extends JpaRepository<User_location,Integer> {
+public interface User_location_repo extends JpaRepository<User_location,String> {
 
 
 
